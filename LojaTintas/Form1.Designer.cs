b8ce5effe2bbb5e45d7fa3d933fc02e8ca804b7c
Gustavo Eric Alves Melo <gugaben28@gmail.com>
2025-04-03 14:13:12 -0300

    Loja tintas

--- LojaTintas/Form1.Designer.cs
+++ LojaTintas/Form1.Designer.cs
@@ -38,16 +38,17 @@
             columnHeader4 = new ColumnHeader();
             txtComprimento = new TextBox();
             label2 = new Label();
-            radioButton1 = new RadioButton();
-            radioButton2 = new RadioButton();
-            radioButton3 = new RadioButton();
+            rbGalaoLata = new RadioButton();
+            rbLata = new RadioButton();
+            rbGalao = new RadioButton();
             SuspendLayout();
             // 
             // btCalcular
             // 
-            btCalcular.Location = new Point(170, 163);
+            btCalcular.Location = new Point(194, 217);
+            btCalcular.Margin = new Padding(3, 4, 3, 4);
             btCalcular.Name = "btCalcular";
-            btCalcular.Size = new Size(201, 23);
+            btCalcular.Size = new Size(230, 31);
             btCalcular.TabIndex = 0;
             btCalcular.Text = "Calcular";
             btCalcular.UseVisualStyleBackColor = true;
@@ -56,26 +57,29 @@
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(66, 64);
+            label1.Location = new Point(75, 85);
             label1.Name = "label1";
-            label1.Size = new Size(80, 15);
+            label1.Size = new Size(104, 20);
             label1.TabIndex = 1;
             label1.Text = "Digite a altura";
             label1.Click += label1_Click;
             // 
             // txtAltura
             // 
-            txtAltura.Location = new Point(56, 115);
+            txtAltura.Location = new Point(64, 153);
+            txtAltura.Margin = new Padding(3, 4, 3, 4);
             txtAltura.Name = "txtAltura";
-            txtAltura.Size = new Size(201, 23);
+            txtAltura.Size = new Size(229, 27);
             txtAltura.TabIndex = 2;
             // 
             // lvlResultados
             // 
             lvlResultados.Columns.AddRange(new ColumnHeader[] { columnHeader1, columnHeader2, columnHeader3, columnHeader4 });
-            lvlResultados.Location = new Point(56, 278);
+            lvlResultados.FullRowSelect = true;
+            lvlResultados.Location = new Point(64, 371);
+            lvlResultados.Margin = new Padding(3, 4, 3, 4);
             lvlResultados.Name = "lvlResultados";
-            lvlResultados.Size = new Size(421, 160);
+            lvlResultados.Size = new Size(791, 212);
             lvlResultados.TabIndex = 3;
             lvlResultados.UseCompatibleStateImageBehavior = false;
             lvlResultados.SelectedIndexChanged += listView1_SelectedIndexChanged;
@@ -98,69 +102,76 @@
             // 
             // txtComprimento
             // 
-            txtComprimento.Location = new Point(276, 115);
+            txtComprimento.Location = new Point(315, 153);
+            txtComprimento.Margin = new Padding(3, 4, 3, 4);
             txtComprimento.Name = "txtComprimento";
-            txtComprimento.Size = new Size(201, 23);
+            txtComprimento.Size = new Size(229, 27);
             txtComprimento.TabIndex = 4;
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(276, 64);
+            label2.Location = new Point(315, 85);
             label2.Name = "label2";
-            label2.Size = new Size(124, 15);
+            label2.Size = new Size(157, 20);
             label2.TabIndex = 5;
             label2.Text = "Digite o comprimento";
             // 
-            // radioButton1
+            // rbGalaoLata
             // 
-            radioButton1.AutoSize = true;
-            radioButton1.Location = new Point(49, 226);
-            radioButton1.Name = "radioButton1";
-            radioButton1.Size = new Size(89, 19);
-            radioButton1.TabIndex = 6;
-            radioButton1.TabStop = true;
-            radioButton1.Text = "Galão e Lata";
-            radioButton1.UseVisualStyleBackColor = true;
-            radioButton1.CheckedChanged += radioButton1_CheckedChanged;
+            rbGalaoLata.AutoSize = true;
+            rbGalaoLata.Location = new Point(56, 301);
+            rbGalaoLata.Margin = new Padding(3, 4, 3, 4);
+            rbGalaoLata.Name = "rbGalaoLata";
+            rbGalaoLata.Size = new Size(113, 24);
+            rbGalaoLata.TabIndex = 6;
+            rbGalaoLata.TabStop = true;
+            rbGalaoLata.Text = "Galão e Lata";
+            rbGalaoLata.UseVisualStyleBackColor = true;
+            rbGalaoLata.CheckedChanged += radioButton1_CheckedChanged;
             // 
-            // radioButton2
+            // rbLata
             // 
-            radioButton2.AutoSize = true;
-            radioButton2.Location = new Point(196, 226);
-            radioButton2.Name = "radioButton2";
-            radioButton2.Size = new Size(89, 19);
-            radioButton2.TabIndex = 7;
-            radioButton2.TabStop = true;
-            radioButton2.Text = "Apenas Lata";
-            radioButton2.UseVisualStyleBackColor = true;
+            rbLata.AutoSize = true;
+            rbLata.Location = new Point(224, 301);
+            rbLata.Margin = new Padding(3, 4, 3, 4);
+            rbLata.Name = "rbLata";
+            rbLata.Size = new Size(111, 24);
+            rbLata.TabIndex = 7;
+            rbLata.TabStop = true;
+            rbLata.Text = "Apenas Lata";
+            rbLata.UseVisualStyleBackColor = true;
+            rbLata.CheckedChanged += rbLata_CheckedChanged;
             // 
-            // radioButton3
+            // rbGalao
             // 
-            radioButton3.AutoSize = true;
-            radioButton3.Location = new Point(356, 226);
-            radioButton3.Name = "radioButton3";
-            radioButton3.Size = new Size(97, 19);
-            radioButton3.TabIndex = 8;
-            radioButton3.TabStop = true;
-            radioButton3.Text = "Apenas Galão";
-            radioButton3.UseVisualStyleBackColor = true;
+            rbGalao.AutoSize = true;
+            rbGalao.Location = new Point(407, 301);
+            rbGalao.Margin = new Padding(3, 4, 3, 4);
+            rbGalao.Name = "rbGalao";
+            rbGalao.Size = new Size(122, 24);
+            rbGalao.TabIndex = 8;
+            rbGalao.TabStop = true;
+            rbGalao.Text = "Apenas Galão";
+            rbGalao.UseVisualStyleBackColor = true;
+            rbGalao.CheckedChanged += rbGalao_CheckedChanged;
             // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             AutoSize = true;
-            ClientSize = new Size(800, 450);
-            Controls.Add(radioButton3);
-            Controls.Add(radioButton2);
-            Controls.Add(radioButton1);
+            ClientSize = new Size(914, 600);
+            Controls.Add(rbGalao);
+            Controls.Add(rbLata);
+            Controls.Add(rbGalaoLata);
             Controls.Add(label2);
             Controls.Add(txtComprimento);
             Controls.Add(lvlResultados);
             Controls.Add(txtAltura);
             Controls.Add(label1);
             Controls.Add(btCalcular);
+            Margin = new Padding(3, 4, 3, 4);
             Name = "Form1";
             Text = "Form1";
             Load += Form1_Load;
@@ -180,8 +191,8 @@
         private ColumnHeader columnHeader4;
         private TextBox txtComprimento;
         private Label label2;
-        private RadioButton radioButton1;
-        private RadioButton radioButton2;
-        private RadioButton radioButton3;
+        private RadioButton rbGalaoLata;
+        private RadioButton rbLata;
+        private RadioButton rbGalao;
     }
 }
--- LojaTintas/Form1.cs
+++ LojaTintas/Form1.cs
@@ -23,39 +23,67 @@ namespace LojaTintas
             lvlResultados.FullRowSelect = true;
             lvlResultados.BackColor = Color.LightBlue;
         }
-
+        string escolha = "";
         private void btCalcular_Click(object sender, EventArgs e)
         {
             float area = float.Parse(txtAltura.Text) * float.Parse(txtComprimento.Text);
-            ListViewItem apenasLatas = new ListViewItem(area.ToString());
+
             double litros = area / 6;
             litros = litros * 1.10;
+
             int apenas_latas = (int)Math.Ceiling(litros / 18);
             int apenas_gal = (int)Math.Ceiling(litros / 3.6);
+
             double preco_apenas_latas = apenas_latas * 80;
             double preco_apenas_gal = apenas_gal * 25;
-            int latas_gal, latas;
+            int galoes, latas;
             latas = (int)litros / 18;
             double sobra = litros % 18;
-            latas_gal = (int)Math.Ceiling(sobra / 3.6);
-
+            galoes = (int)Math.Ceiling(sobra / 3.6);
+            if (escolha == "Gal√£o e Lata")
+            {
+                ListViewItem galLata = new ListViewItem(area.ToString());
+                galLata.SubItems.Add("Gal√£o e Lata");
+                galLata.SubItems.Add("Latas de 18L: " + latas+ " e Gal√µes de 3.6L: "+galoes);
+                galLata.SubItems.Add("R$ " + (latas * 80 + galoes * 25).ToString() + ",00");
+                galLata.BackColor = Color.Green;
+                lvlResultados.Items.Add(galLata);
+            }
+            else if (escolha == "Apenas Gal√£o")
+            {
+                ListViewItem apenasGalao = new ListViewItem(area.ToString());
+                apenasGalao.SubItems.Add("Apenas Gal√£o");
+                apenasGalao.SubItems.Add("Gal√µes de 3.6L: " + apenas_gal);
+                apenasGalao.SubItems.Add("R$ " + preco_apenas_gal.ToString() + ",00");
+                apenasGalao.BackColor = Color.Yellow;
+                lvlResultados.Items.Add(apenasGalao);
+            }
+            else if(escolha == "Apenas Lata")
+            { 
+                ListViewItem apenasLatas = new ListViewItem(area.ToString());
             apenasLatas.SubItems.Add("Apenas Latas");
             apenasLatas.SubItems.Add("Latas de 18L: " + apenas_latas);
-            apenasLatas.SubItems.Add("R$ "+preco_apenas_latas.ToString()+",00");
+            apenasLatas.SubItems.Add("R$ " + preco_apenas_latas.ToString() + ",00");
             apenasLatas.BackColor = Color.Red;
             lvlResultados.Items.Add(apenasLatas);
-            //lvlResultados.Items.Add("Galıes de 3.6L: " + apenas_gal);
-            //lvlResultados.Items.Add("PreÁo Latas: " + preco_lata);
-            //lvlResultados.Items.Add("PreÁo Galıes: " + preco_gal);
-            //lvlResultados.Items.Add("Latas de 18L: " + latas);
-            //lvlResultados.Items.Add("Galıes de 3.6L: " + latas_gal);
+            }
 
 
         }
 
         private void radioButton1_CheckedChanged(object sender, EventArgs e)
         {
+            escolha = "Gal√£o e Lata";
+        }
 
+        private void rbLata_CheckedChanged(object sender, EventArgs e)
+        {
+            escolha = "Apenas Lata";
+        }
+
+        private void rbGalao_CheckedChanged(object sender, EventArgs e)
+        {
+            escolha = "Apenas Gal√£o";   
         }
     }
 }
